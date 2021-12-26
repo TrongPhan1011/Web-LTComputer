22fedead8e930c59474e77047ee91cf19e36fe00
OS10.13 <os10.13@TP-Lucifer.local>
2021-12-26 19:16:40 +0700

    update data

---
+++ js/data/dsPhuKien.js
@@ -0,0 +1,144 @@
+const dsPhuKien = [
+    {
+        maSP : 1,
+        tenSP : 'Tai nghe AirPods Pro 2021',
+        loaiSP: 'Tai nghe',
+        moTa : 'AirPods Pro 2021 sẽ giúp bạn cảm nhận trọn vẹn những giá trị làm nên tên tuổi của dòng tai nghe Apple như sự tinh xảo, độ nhỏ gọn và tính đa dụng thông qua hộp sạc đặc trưng. Sản phẩm sử dụng chip H1 thông minh và tăng cường chất âm thông qua Adaptive EQ. Những cải tiến hiệu quả trong phiên bản mới giúp AirPods Pro 2021 tương thích tốt với công nghệ sạc MagSafe.',
+        gia : 5399900,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp5/1',
+            '../assets/img/sp5/2',
+            '../assets/img/sp5/3',
+            '../assets/img/sp5/4'
+        ]
+    },
+    {
+        maSP : 2,
+        tenSP : 'Tai nghe AirPods Pro 2021',
+        loaiSP: 'Tai nghe',
+        moTa : 'AirPods Pro 2021 sẽ giúp bạn cảm nhận trọn vẹn những giá trị làm nên tên tuổi của dòng tai nghe Apple như sự tinh xảo, độ nhỏ gọn và tính đa dụng thông qua hộp sạc đặc trưng. Sản phẩm sử dụng chip H1 thông minh và tăng cường chất âm thông qua Adaptive EQ. Những cải tiến hiệu quả trong phiên bản mới giúp AirPods Pro 2021 tương thích tốt với công nghệ sạc MagSafe.',
+        gia : 4399900,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp5/1',
+            '../assets/img/sp5/2',
+            '../assets/img/sp5/3',
+            '../assets/img/sp5/4'
+        ]
+    },
+    {
+        maSP : 3,
+        tenSP : 'Tai nghe AirPods Pro 2021',
+        loaiSP: 'Tai nghe',
+        moTa : 'AirPods Pro 2021 sẽ giúp bạn cảm nhận trọn vẹn những giá trị làm nên tên tuổi của dòng tai nghe Apple như sự tinh xảo, độ nhỏ gọn và tính đa dụng thông qua hộp sạc đặc trưng. Sản phẩm sử dụng chip H1 thông minh và tăng cường chất âm thông qua Adaptive EQ. Những cải tiến hiệu quả trong phiên bản mới giúp AirPods Pro 2021 tương thích tốt với công nghệ sạc MagSafe.',
+        gia : 6399900,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp5/1',
+            '../assets/img/sp5/2',
+            '../assets/img/sp5/3',
+            '../assets/img/sp5/4'
+        ]
+    },
+    {
+        maSP : 4,
+        tenSP : 'Pin sạc dự phòng PowerGo Smart Ai',
+        loaiSP: 'Sạc dự phòng',
+        moTa : 'Với thiết kế nhỏ gọn như một smartphone, hỗ trợ hai cổng nguồn ra USB-A và ghi nhận dung lượng pin lên tới 10.000 mAh, sản phẩm pin sạc dự phòng PowerGo Smart AI Innostyle sẽ hỗ trợ rất nhiều cho bạn trong quá trình sử dụng điện thoại, đặc biệt là cho những chuyến đi xa và cần duy trì nguồn năng lượng cho nhiều thiết bị thông minh.',
+        gia : 299000,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp6/1',
+            '../assets/img/sp6/2',
+            '../assets/img/sp6/3',
+            '../assets/img/sp6/4'
+        ]
+    },
+    {
+        maSP : 5,
+        tenSP : 'Pin sạc dự phòng PowerGo Smart Ai',
+        loaiSP: 'Sạc dự phòng',
+        moTa : 'Với thiết kế nhỏ gọn như một smartphone, hỗ trợ hai cổng nguồn ra USB-A và ghi nhận dung lượng pin lên tới 10.000 mAh, sản phẩm pin sạc dự phòng PowerGo Smart AI Innostyle sẽ hỗ trợ rất nhiều cho bạn trong quá trình sử dụng điện thoại, đặc biệt là cho những chuyến đi xa và cần duy trì nguồn năng lượng cho nhiều thiết bị thông minh.',
+        gia : 199000,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp6/1',
+            '../assets/img/sp6/2',
+            '../assets/img/sp6/3',
+            '../assets/img/sp6/4'
+        ]
+    },
+    {
+        maSP : 6,
+        tenSP : 'Pin sạc dự phòng PowerGo Smart Ai',
+        loaiSP: 'Sạc dự phòng',
+        moTa : 'Với thiết kế nhỏ gọn như một smartphone, hỗ trợ hai cổng nguồn ra USB-A và ghi nhận dung lượng pin lên tới 10.000 mAh, sản phẩm pin sạc dự phòng PowerGo Smart AI Innostyle sẽ hỗ trợ rất nhiều cho bạn trong quá trình sử dụng điện thoại, đặc biệt là cho những chuyến đi xa và cần duy trì nguồn năng lượng cho nhiều thiết bị thông minh.',
+        gia : 259000,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp6/1',
+            '../assets/img/sp6/2',
+            '../assets/img/sp6/3',
+            '../assets/img/sp6/4'
+        ]
+    },
+    {
+        maSP : 7,
+        tenSP : 'Pin sạc dự phòng PowerGo Smart Ai',
+        loaiSP: 'Sạc dự phòng',
+        moTa : 'Với thiết kế nhỏ gọn như một smartphone, hỗ trợ hai cổng nguồn ra USB-A và ghi nhận dung lượng pin lên tới 10.000 mAh, sản phẩm pin sạc dự phòng PowerGo Smart AI Innostyle sẽ hỗ trợ rất nhiều cho bạn trong quá trình sử dụng điện thoại, đặc biệt là cho những chuyến đi xa và cần duy trì nguồn năng lượng cho nhiều thiết bị thông minh.',
+        gia : 399000,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp6/1',
+            '../assets/img/sp6/2',
+            '../assets/img/sp6/3',
+            '../assets/img/sp6/4'
+        ]
+    },
+    {
+        maSP : 8,
+        tenSP : 'Loa Bluetooth Sony SRS-XB13',
+        loaiSP: 'Loa',
+        moTa : 'Sức mạnh cực đỉnh với bộ vi xử lý AMD Ryzen 5500U 6 lõi 12 luồng, đi cùng 8GB RAM, 512GB SSD và màn hình Full HD sắc nét, HP 245 G8 là chiếc laptop di động đầy ấn tượng dành cho bạn.',
+        gia : 1499900,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp7/1',
+            '../assets/img/sp7/2',
+            '../assets/img/sp7/3',
+            '../assets/img/sp7/4'
+        ]
+    }
+    ,
+    {
+        maSP : 9,
+        tenSP : 'Loa Bluetooth Sony SRS-XB13',
+        loaiSP: 'Loa',
+        moTa : 'Sức mạnh cực đỉnh với bộ vi xử lý AMD Ryzen 5500U 6 lõi 12 luồng, đi cùng 8GB RAM, 512GB SSD và màn hình Full HD sắc nét, HP 245 G8 là chiếc laptop di động đầy ấn tượng dành cho bạn.',
+        gia : 1299900,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp7/1',
+            '../assets/img/sp7/2',
+            '../assets/img/sp7/3',
+            '../assets/img/sp7/4'
+        ]
+        
+    },
+    {
+        maSP : 10,
+        tenSP : 'Loa Bluetooth Sony SRS-XB13',
+        loaiSP: 'Loa',
+        moTa : 'Sức mạnh cực đỉnh với bộ vi xử lý AMD Ryzen 5500U 6 lõi 12 luồng, đi cùng 8GB RAM, 512GB SSD và màn hình Full HD sắc nét, HP 245 G8 là chiếc laptop di động đầy ấn tượng dành cho bạn.',
+        gia : 1399900,
+        soLuong: 100,
+        urlAnh: [
+            '../assets/img/sp7/1',
+            '../assets/img/sp7/2',
+            '../assets/img/sp7/3',
+            '../assets/img/sp7/4'
+        ]
+    }
+]
